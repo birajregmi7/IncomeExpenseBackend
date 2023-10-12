1360eb15c40545505ccbf68fb5a4ed093f122911
Adarsh Dhital Khatri <biraj.regmi.jr@gmail.com>
2023-10-13 01:53:21 +0530

    Updated

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const url = 'mongodb://localhost:27017';
+const url = process.env.MONGODB_URI; 
 
 app.get('/api/transactions', async (req, res) => {
   try {
